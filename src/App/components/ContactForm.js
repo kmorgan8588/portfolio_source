ddc3f3f51759a396c7b03584975466614c374e42
Kyle Morgan <kmorgan8588@gmail.com>
2020-08-04 15:12:54 -0700

    adding custom form hook to handle all form field updates and form logic

--- src/App/components/ContactForm.js
+++ src/App/components/ContactForm.js
@@ -1,17 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import useForm from '../Hooks/useForm';
 
 
 const ContactForm = (props) => {
   const { view } = props;
+  const log = () => {
+    console.log(values);
+    reset();
+  }
+  const { values, handleSubmit, handleChange, reset } = useForm(log);
+
+
+
    return view === 'form' ? (
    <div className="form-style">
-      <form method="POST" action="/email" autoComplete="off">
+      <form onSubmit={handleSubmit}>
         <fieldset>
         <legend>Email Info</legend>
-        <input type="text" name="name" placeholder="Your Name *" required/>
-        <input type="text" name="subject" placeholder="Subject *" required/>
-        <input type="email" name="email" placeholder="Your Email *" required/>
-        <textarea name="message" placeholder="Message" required></textarea>
+        <input type="text" name="name" placeholder="Your Name *" value={values.name || ""} onChange={handleChange} required/>
+        <input type="text" name="subject" placeholder="Subject *" value={values.subject || ""} onChange={handleChange} required/>
+        <input type="email" name="email" placeholder="Your Email *" value={values.email || ""} onChange={handleChange} required/>
+        <textarea name="message" placeholder="Message" value={values.message || ""} onChange={handleChange} required></textarea>
       </fieldset>
       <input type="submit" value="Send" />
       </form>
