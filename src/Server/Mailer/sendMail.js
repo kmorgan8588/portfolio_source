c43d4e0f63ffd442854f13cc620b3cb4728651ee
Kyle Morgan <kmorgan8588@gmail.com>
2020-08-08 09:43:53 -0700

    splitting projects into projectslist for adding second view
    going to add contributions to open source as a new sublink for project

--- src/Server/Mailer/sendMail.js
+++ src/Server/Mailer/sendMail.js
@@ -1,14 +1,11 @@
 const transporter = require('./transport');
 
-
 module.exports = sendMail = (mailOptions, res) => {
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
-      console.log(err);
       res.status(500).json('fail');
     } else {
-      console.log('Email sent: ', info.response);
       res.status(200).json('success');
     }
   })
-}
+};
