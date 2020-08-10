0051d6ca4d4d9c5c0932f651abd3c7ea3d0bcc59
Kyle Morgan <kmorgan8588@gmail.com>
2020-08-10 13:57:12 -0700

    add noMatch route "*" for accidental navigation outside bounds of the app
    Included a list of current technologies used on the home page

--- src/App/components/Home.js
+++ src/App/components/Home.js
@@ -9,6 +9,16 @@ const Home = (props) => (
     Regarding this site: If you're interested in viewing the source code or see something that needs to be fixed, visit the site's {' '}
       <a target="_blank" href="https://github.com/kmorgan8588/portfolio_source">GitHub repo</a>.
      </p>
+     <h4>Technologies Used:</h4>
+     <ul>
+       <li>React</li>
+       <li>React-Router</li>
+       <li>Express</li>
+       <li>NodeMailer</li>
+       <li>Docker</li>
+       <li>Nginx</li>
+       <li>AWS</li>
+     </ul>
   </div>
 )
 
